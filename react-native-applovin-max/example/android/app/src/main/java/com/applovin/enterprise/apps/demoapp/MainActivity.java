c359901a440d46a1da9029bdb8d81f19606a573c
Thomas So <thomas.m.so33@gmail.com>
2020-07-11 17:11:58 -0700

    WIP

--- react-native-applovin-max/example/android/app/src/main/java/com/applovin/enterprise/apps/demoapp/MainActivity.java
+++ react-native-applovin-max/example/android/app/src/main/java/com/applovin/enterprise/apps/demoapp/MainActivity.java
@@ -1,15 +1,17 @@
-package com.example.reactnativeapplovinmax;
+package com.applovin.enterprise.apps.demoapp;
 
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
-
+public class MainActivity
+  extends ReactActivity
+{
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   @Override
-  protected String getMainComponentName() {
+  protected String getMainComponentName()
+  {
     return "ApplovinMaxExample";
   }
 }
