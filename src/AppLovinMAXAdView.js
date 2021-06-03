fefecb975286bbba1e978f40e23f55c6829e7a17
Harry Arakkal <harry.arakkal@applovin.com>
2021-06-03 15:04:20 -0700

    Bug/mrec_not_clickable (#37)

--- src/AppLovinMAXAdView.js
+++ src/AppLovinMAXAdView.js
@@ -1,10 +1,23 @@
 import { requireNativeComponent } from "react-native";
 import PropTypes from "prop-types";
 import React from "react";
+import AppLovinMAX from "./index.js";
 
 class AdView extends React.Component {
   render() {
-    return <AppLovinMAXAdView {...this.props} />;
+    let { style, ...otherProps } = this.props;
+    return <AppLovinMAXAdView
+            // This merges the pub-specified style with ours, but overwrites width/height.
+            style={Object.assign({}, style, this.sizeForAdFormat(otherProps.adFormat))}
+            {...otherProps} />;
+  }
+  
+  sizeForAdFormat(adFormat) {
+      if (adFormat == AppLovinMAX.AdFormat.BANNER) {
+          return AppLovinMAX.isTablet() ? {width: 728, height: 90} : {width: 320, height: 50}
+      } else {
+          return {width: 300, height: 250}
+      }
   }
 }
 
