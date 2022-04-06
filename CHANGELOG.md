582fce72e735b415c39c087e9dd89ba8c3ed5870
Thomas So <thomas.m.so33@gmail.com>
2022-04-05 21:29:22 -0700

    3.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 ## Versions
 
-## x.x.x
+## 3.0.2
     * Lock to Android SDK 11.3.2 and iOS SDK 11.3.2.
 ## 3.0.1
     * Lock to Android SDK 11.3.1 and iOS SDK 11.3.1.
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import AdView from "./AppLovinMAXAdView";
 
 const { AppLovinMAX } = NativeModules;
 
-const VERSION = "3.0.1";
+const VERSION = "3.0.2";
 
 /**
  * This enum represents whether or not the consent dialog should be shown for this user.
