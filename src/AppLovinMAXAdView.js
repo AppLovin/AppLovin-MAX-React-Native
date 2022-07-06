6b2251cf1ef74bc52dd4028678a39a799163b3fc
Hiroshi <100324169+alhiwatan@users.noreply.github.com>
2022-07-06 14:16:05 +0900

    Update src/AppLovinMAXAdView.js
    Co-authored-by: Thomas So <5104410+thomasmso@users.noreply.github.com>

--- src/AppLovinMAXAdView.js
+++ src/AppLovinMAXAdView.js
@@ -38,7 +38,7 @@ const AdViewWrapper = (props) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     flexDirection: 'column',
     justifyContent: 'flex-end',
     alignItems: 'center',
