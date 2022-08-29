32412d3eddcd032592eaf656747376b3eed0ca2d
Hiroshi Watanabe <hiroshi.watanabe@applovin.com>
2022-08-29 17:01:44 +0900

    Updated the iOS implementation

--- ios/AppLovinMAXAdView.m
+++ ios/AppLovinMAXAdView.m
@@ -4,14 +4,16 @@
 
 @interface AppLovinMAXAdView ()
 
-@property (nonatomic, strong) MAAdView *mAdView;
-@property (nonatomic, weak) MAAdFormat *mAdFormat;
+@property (nonatomic, strong) MAAdView *adView;
 
-@property (nonatomic, copy) NSString *mAdUnitId;
-@property (nonatomic, copy) NSString *mPlacement;
-@property (nonatomic, copy) NSString *mCustomData;
-@property (nonatomic, assign) BOOL mAdaptiveBannerEnabled;
-@property (nonatomic, assign) BOOL mAutoRefresh;
+// Properties that are updated from the correspondig Javascript
+// properties via ViewManager
+@property (nonatomic, copy) NSString *adUnitId;
+@property (nonatomic, weak) MAAdFormat *adFormat;
+@property (nonatomic, copy) NSString *placement;
+@property (nonatomic, copy) NSString *customData;
+@property (nonatomic, assign, readonly, getter=isAdaptiveBannerEnabled) BOOL adaptiveBannerEnabled;
+@property (nonatomic, assign, readonly, getter=isAutoRefreshEnabled) BOOL autoRefresh;
 
 @end
 
@@ -19,24 +21,24 @@
 
 - (void)setAdUnitId:(NSString *)adUnitId
 {
-    if ( self.mAdView ) return;
+    if ( self.adView ) return;
     
-    self.mAdUnitId = adUnitId;
+    _adUnitId = adUnitId;
     
     [self attachAdView];
 }  
 
 - (void)setAdFormat:(NSString *)adFormat
 {
-    if ( self.mAdView ) return;
+    if ( self.adView ) return;
     
     if ( [@"banner" isEqualToString: adFormat] )
     {
-        self.mAdFormat = DEVICE_SPECIFIC_ADVIEW_AD_FORMAT;
+        _adFormat = DEVICE_SPECIFIC_ADVIEW_AD_FORMAT;
     }
     else if ( [@"mrec" isEqualToString: adFormat] )
     {
-        self.mAdFormat = MAAdFormat.mrec;
+        _adFormat = MAAdFormat.mrec;
     }
     
     [self attachAdView];
@@ -44,110 +46,125 @@
 
 - (void)setPlacement:(NSString *)placement
 {
-    self.mPlacement = placement;
+    _placement = placement;
     
-    if ( self.mAdView )
+    if ( self.adView )
     {
-        self.mAdView.placement = placement;
+        self.adView.placement = placement;
     }
 }
 
 - (void)setCustomData:(NSString *)customData
 {
-    self.mCustomData = customData;
+    _customData = customData;
     
-    if ( self.mAdView )
+    if ( self.adView )
     {
-        self.mAdView.customData = customData;
+        self.adView.customData = customData;
     }
 }
 
 - (void)setAdaptiveBannerEnabled:(BOOL)adaptiveBannerEnabled
 {
-    self.mAdaptiveBannerEnabled = adaptiveBannerEnabled;
+    _adaptiveBannerEnabled = adaptiveBannerEnabled;
     
-    if ( self.mAdView )
+    if ( self.adView )
     {
-        [self.mAdView setExtraParameterForKey: @"adaptive_banner" value: (adaptiveBannerEnabled ? @"YES" : @"NO")];
+        if ( adaptiveBannerEnabled )
+        {
+            [self.adView setExtraParameterForKey: @"adaptive_banner" value: @"true"];
+        }
+        else
+        {
+            [self.adView setExtraParameterForKey: @"adaptive_banner" value: @"false"];
+        }
     }
 }
 
 - (void)setAutoRefresh:(BOOL)autoRefresh
 {
-    self.mAutoRefresh = autoRefresh;
+    _autoRefresh = autoRefresh;
     
-    if ( self.mAdView )
+    if ( self.adView )
     {
         if ( autoRefresh )
         {
-            [self.mAdView startAutoRefresh];
+            [self.adView startAutoRefresh];
         }
         else
         {
-            [self.mAdView stopAutoRefresh];
+            [self.adView stopAutoRefresh];
         }
     }
 }
 
 - (void)attachAdView
 {
-    // This dispatch_async allows finishing to get all properties
-    dispatch_async(dispatch_get_main_queue(), ^{
+    // Run after 0.25 sec delay to allow all properties to set
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (NSEC_PER_SEC/4)), dispatch_get_main_queue(), ^{
+        if ( self.adView ) return;
         
-        if ( self.mAdView ) return;
-
+        RCTLogInfo(@"[AppLovinSdk] [AppLovinMAX] attempting to re-attach ad view %@", self.adUnitId);
+        
         // If ad unit id and format has been set - create and attach AdView
-        if ( !( [self.mAdUnitId al_isValidString] && self.mAdFormat )) return;
+        if ( !( [self.adUnitId al_isValidString] && self.adFormat )) return;
         
-        self.mAdView = [[MAAdView alloc] initWithAdUnitIdentifier: self.mAdUnitId adFormat: self.mAdFormat sdk: AppLovinMAX.shared.sdk];
-        self.mAdView.frame = (CGRect) { CGPointZero, self.mAdFormat.size };
-        self.mAdView.delegate = AppLovinMAX.shared; // Go through core class for callback forwarding to React Native layer
-        self.mAdView.revenueDelegate = AppLovinMAX.shared;
+        self.adView = [[MAAdView alloc] initWithAdUnitIdentifier: self.adUnitId adFormat: self.adFormat sdk: AppLovinMAX.shared.sdk];
+        self.adView.frame = (CGRect) { CGPointZero, self.adFormat.size };
+        self.adView.delegate = AppLovinMAX.shared; // Go through core class for callback forwarding to React Native layer
+        self.adView.revenueDelegate = AppLovinMAX.shared;
         
-        self.mAdView.placement = self.mPlacement;
+        self.adView.placement = self.placement;
         
-        self.mAdView.customData = self.mCustomData;
+        self.adView.customData = self.customData;
         
-        [self.mAdView setExtraParameterForKey: @"adaptive_banner" value: (self.mAdaptiveBannerEnabled ? @"true" : @"false")];
-        
-        // Set this extra parameter to work around a SDK bug that ignores calls to stopAutoRefresh()
-        [self.mAdView setExtraParameterForKey: @"allow_pause_auto_refresh_immediately" value: @"true"];
-        
-        if ( self.mAutoRefresh )
+        if ( [self isAdaptiveBannerEnabled] )
         {
-            [self.mAdView startAutoRefresh];
+            [self.adView setExtraParameterForKey: @"adaptive_banner" value: @"true"];
         }
         else
         {
-            [self.mAdView stopAutoRefresh];
+            [self.adView setExtraParameterForKey: @"adaptive_banner" value: @"false"];
         }
         
-        [self.mAdView loadAd];
+        // Set this extra parameter to work around a SDK bug that ignores calls to stopAutoRefresh()
+        [self.adView setExtraParameterForKey: @"allow_pause_auto_refresh_immediately" value: @"true"];
         
-        [self addSubview: self.mAdView];
+        if ( [self isAutoRefreshEnabled] )
+        {
+            [self.adView startAutoRefresh];
+        }
+        else
+        {
+            [self.adView stopAutoRefresh];
+        }
         
-        CGSize adViewSize = [self.mAdFormat adaptiveSizeForWidth: CGRectGetWidth(self.frame)];
-        [NSLayoutConstraint activateConstraints: @[[self.mAdView.widthAnchor constraintEqualToConstant: adViewSize.width],
-                                                   [self.mAdView.heightAnchor constraintEqualToConstant: adViewSize.height],
-                                                   [self.mAdView.centerXAnchor constraintEqualToAnchor: self.centerXAnchor],
-                                                   [self.mAdView.centerYAnchor constraintEqualToAnchor: self.centerYAnchor]]];
+        [self.adView loadAd];
+        
+        [self addSubview: self.adView];
+        
+        CGSize adViewSize = [self.adFormat adaptiveSizeForWidth: CGRectGetWidth(self.frame)];
+        [NSLayoutConstraint activateConstraints: @[[self.adView.widthAnchor constraintEqualToConstant: adViewSize.width],
+                                                   [self.adView.heightAnchor constraintEqualToConstant: adViewSize.height],
+                                                   [self.adView.centerXAnchor constraintEqualToAnchor: self.centerXAnchor],
+                                                   [self.adView.centerYAnchor constraintEqualToAnchor: self.centerYAnchor]]];
         
     });
 }
 
-// Called when the subview’s superview changes or when the subview is
-// removed from the view hierarchy completely.
-- (void)willRemoveSubview:(UIView *)subview
+- (void)didMoveToWindow
 {
-    // This view is being removed
-    if ( subview == nil )
+    [super didMoveToWindow];
+
+    // This view is unmounted
+    if ( !self.window )
     {
-        if ( self.mAdView )
+        if ( self.adView )
         {
-            self.mAdView.delegate = nil;
-            self.mAdView.revenueDelegate = nil;
+            self.adView.delegate = nil;
+            self.adView.revenueDelegate = nil;
             
-            [self.mAdView removeFromSuperview];
+            [self.adView removeFromSuperview];
         }
     }
 }
