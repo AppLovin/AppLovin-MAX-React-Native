c0cc712a4ff56cdb7ec0f272440c3e10d5a0dfb9
Hiroshi <hiroshi.watanabe@applovin.com>
2023-02-16 16:20:32 +0900

    Update the build environment to support React Native 0.70

--- example/ios/AppLovinMAXExample/AppDelegate.m
+++ example/ios/AppLovinMAXExample/AppDelegate.m
@@ -54,7 +54,7 @@
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
