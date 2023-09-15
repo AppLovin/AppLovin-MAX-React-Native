238c7afa434c66bed84dbfcaa8ace0e730ce70c0
Hiroshi <100324169+alhiwatan@users.noreply.github.com>
2023-09-15 11:31:06 +0900

    5.7.2 (#243)

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import EventListeners from "./AppLovinMAXEventListeners";
 
 const { AppLovinMAX } = NativeModules;
 
-const VERSION = "5.7.1";
+const VERSION = "5.7.2";
 
 /*---------*/
 /* BANNERS */
