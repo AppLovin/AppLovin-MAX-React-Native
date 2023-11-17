d48b2d147744c73999f65cc2a0258c848aebe6f4
Hiroshi <100324169+alhiwatan@users.noreply.github.com>
2023-11-17 18:35:00 +0900

    ref/update_nativead_asset_views_with_textprops (#270)
    * fix Text Component Props ViewProps to TextProps
    
    * Update CallToActionView with TextProps
    
    ---------
    
    Co-authored-by: kimtaehyun-minding <kth4350@minding.today>

--- src/nativeAd/NativeAdViewComponents.tsx
+++ src/nativeAd/NativeAdViewComponents.tsx
@@ -1,9 +1,10 @@
-import React, { useContext, useRef, useEffect, type ReactNode } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
+import type { ReactNode } from 'react';
 import { findNodeHandle, Text, Image, View, TouchableOpacity, StyleSheet } from 'react-native';
-import type { ViewProps, ImageProps, TextStyle, StyleProp } from 'react-native';
+import type { ViewProps, ImageProps, TextStyle, StyleProp, TextProps } from 'react-native';
 import { NativeAdViewContext } from './NativeAdViewProvider';
 
-export const TitleView = (props: ViewProps) => {
+export const TitleView = (props: TextProps) => {
     const titleRef = useRef(null);
     const { nativeAd, nativeAdView } = useContext(NativeAdViewContext);
 
@@ -22,7 +23,7 @@ export const TitleView = (props: ViewProps) => {
     );
 };
 
-export const AdvertiserView = (props: ViewProps) => {
+export const AdvertiserView = (props: TextProps) => {
     const advertiserRef = useRef(null);
     const { nativeAd, nativeAdView } = useContext(NativeAdViewContext);
 
@@ -41,7 +42,7 @@ export const AdvertiserView = (props: ViewProps) => {
     );
 };
 
-export const BodyView = (props: ViewProps) => {
+export const BodyView = (props: TextProps) => {
     const bodyRef = useRef(null);
     const { nativeAd, nativeAdView } = useContext(NativeAdViewContext);
 
@@ -60,7 +61,7 @@ export const BodyView = (props: ViewProps) => {
     );
 };
 
-export const CallToActionView = (props: ViewProps) => {
+export const CallToActionView = (props: TextProps) => {
     const callToActionRef = useRef(null);
     const { nativeAd, nativeAdView } = useContext(NativeAdViewContext);
 
@@ -73,7 +74,7 @@ export const CallToActionView = (props: ViewProps) => {
     }, [nativeAd]);
 
     return (
-        <TouchableOpacity {...props}>
+        <TouchableOpacity>
             <Text {...props} ref={callToActionRef}>
                 {nativeAd.callToAction || null}
             </Text>
